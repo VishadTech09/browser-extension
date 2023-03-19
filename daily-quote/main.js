0e5d8cb057eb6d2d0eb79cd572d58b24fb733374
Vishad Patel <pat.vishad@gmail.com>
2023-03-19 22:46:30 +0530

    updated author photo for quote

--- daily-quote/main.js
+++ daily-quote/main.js
@@ -1,15 +1,19 @@
-
-fetch("https://type.fit/api/quotes")
+//"https://type.fit/api/quotes"
+fetch("https://api.quotable.io/random")
     .then(response => response.json())
     .then(data => {
-        if (data.length) {
-            const quoteItem = data[Math.floor(Math.random() * data.length)];
-            const textElement = document.getElementById("quoteText");
-            const authorElement = document.getElementById("author");
-            textElement.innerHTML = quoteItem.text;
-            authorElement.innerText = quoteItem.author;
-        }
 
+
+        const textElement = document.getElementById("quoteText");
+        const tagsElement = document.getElementById("tags");
+        const authorElement = document.getElementById("author");
+        const authorPic = document.getElementById("authorImg");
+        textElement.innerHTML = data.content;
+        authorElement.innerText = data.author;
+        tagsElement.innerText = data.tags.join(",")
+        const imgUrl = `https://images.quotable.dev/profile/400/${data.authorSlug}.jpg`
+        authorPic.setAttribute("src", imgUrl);
+        authorPic.setAttribute("style", "display:visible");
     })
     .catch(error => {
         document.writeln("Error getting data...")
